f2e31f1b2e366b2cf4bfe93b41e63da79e5d8897
Marianna Shvartsapel <marianna.shvartsapel@contentcritical.com>
2019-03-27 19:05:36 -0400

    add utils and API functions to work with data

---
+++ client/src/utils/api.js
@@ -0,0 +1,10 @@
+import { _getCampaigns, _getCards } from './_data';
+
+export function getInitialData() {
+	return Promise.all([_getCampaigns(), _getCards()]).then(
+		([campaigns, cards]) => ({
+			campaigns,
+			cards
+		})
+	);
+}
