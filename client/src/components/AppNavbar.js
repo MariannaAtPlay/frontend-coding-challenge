926569908e22241b0006a75d872712fd1cd875d9
Marianna Shvartsapel <marianna.shvartsapel@contentcritical.com>
2019-03-27 18:36:58 -0400

    align NavBar's content with the page's content

--- client/src/components/AppNavbar.js
+++ client/src/components/AppNavbar.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {
+	Container,
 	Collapse,
 	Navbar,
 	NavbarToggler,
@@ -41,60 +42,66 @@ export default class Navigation extends React.Component {
 				expand="sm"
 				className="border-bottom border-top mt-2 px-5 py-3"
 			>
-				<NavbarToggler onClick={this.toggle} />
-				<Collapse isOpen={this.state.isOpen} navbar>
-					{/*Pull left */}
-					<Nav className="mr-auto" navbar>
-						<UncontrolledDropdown nav inNavbar className="border">
-							<DropdownToggle
+				<Container>
+					<NavbarToggler onClick={this.toggle} />
+					<Collapse isOpen={this.state.isOpen} navbar>
+						{/*Pull left */}
+						<Nav className="mr-auto" navbar>
+							<UncontrolledDropdown
 								nav
-								caret
-								className="text-dark px-3"
-								style={{ fontSize: 'larger' }}
+								inNavbar
+								className="border"
 							>
-								All Campaigns
-							</DropdownToggle>
-							<DropdownMenu right>
-								<DropdownItem>Option 1</DropdownItem>
-								<DropdownItem>Option 2</DropdownItem>
-							</DropdownMenu>
-						</UncontrolledDropdown>
+								<DropdownToggle
+									nav
+									caret
+									className="text-dark px-3"
+									style={{ fontSize: 'larger' }}
+								>
+									All Campaigns
+								</DropdownToggle>
+								<DropdownMenu right>
+									<DropdownItem>Option 1</DropdownItem>
+									<DropdownItem>Option 2</DropdownItem>
+								</DropdownMenu>
+							</UncontrolledDropdown>
 
-						<NavLink
-							tag="li"
-							className="ml-3 d-flex flex-nowrap align-items-center"
-						>
-							<IoIosList className="mr-3" />
-							Pending
-						</NavLink>
-					</Nav>
-
-					{/* Pull right */}
-					<Nav className="ml-auto" navbar>
-						<NavLink tag="li">
-							<IoIosSearch />
-						</NavLink>
-
-						<NavLink
-							tag="li"
-							className="d-flex flex-nowrap text-nowrap align-items-center"
-							id="calendar"
-						>
-							<IoIosArrowBack className="mr-3" size={20} />
-							<IoIosCalendar className="mr-1" size={20} />
-							Today, {moment().format('MMM Do')}
-							<IoIosArrowForward className="ml-3" size={20} />
-							<Badge
+							<NavLink
 								tag="li"
-								pill
-								id="calendar-step"
-								className="ml-3 px-3 py-2 align-items-center"
+								className="ml-3 d-flex flex-nowrap align-items-center"
 							>
-								1d
-							</Badge>
-						</NavLink>
-					</Nav>
-				</Collapse>
+								<IoIosList className="mr-3" />
+								Pending
+							</NavLink>
+						</Nav>
+
+						{/* Pull right */}
+						<Nav className="ml-auto" navbar>
+							<NavLink tag="li">
+								<IoIosSearch />
+							</NavLink>
+
+							<NavLink
+								tag="li"
+								className="d-flex flex-nowrap text-nowrap align-items-center"
+								id="calendar"
+							>
+								<IoIosArrowBack className="mr-4" size={20} />
+								<IoIosCalendar className="mr-1" size={20} />
+								Today, {moment().format('MMM Do')}
+								<IoIosArrowForward className="ml-4" size={20} />
+								<Badge
+									tag="li"
+									pill
+									id="calendar-step"
+									className="ml-3 px-3 py-2 align-items-center"
+								>
+									1d
+								</Badge>
+							</NavLink>
+						</Nav>
+					</Collapse>
+				</Container>
 			</Navbar>
 		);
 	};
